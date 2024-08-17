2e304b0824ed9bf9bf2ad70f9f4b542a6735f2ea
hashfunc <root@hashfunc.io>
2024-08-18 00:14:05 +0900

    Update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,45 +1,53 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local settings = {
-    color_scheme = {
-        name = "Ef-Day",
-        config = wezterm.color.get_builtin_schemes()["Ef-Day"]
-    }
+local settings = (function()
+	local color_scheme = {
+		name = "Ef-Day",
+	}
+	color_scheme.config = wezterm.color.get_builtin_schemes()[color_scheme.name]
+
+	return {
+		color_scheme = color_scheme,
+	}
+end)()
+
+return {
+	color_scheme = settings.color_scheme.name,
+	colors = {
+		tab_bar = {
+			background = settings.color_scheme.config.background,
+			active_tab = {
+				bg_color = settings.color_scheme.config.background,
+				fg_color = settings.color_scheme.config.foreground,
+			},
+			inactive_tab = {
+				bg_color = settings.color_scheme.config.background,
+				fg_color = settings.color_scheme.config.foreground,
+			},
+			inactive_tab_hover = {
+				bg_color = settings.color_scheme.config.background,
+				fg_color = settings.color_scheme.config.foreground,
+			},
+			inactive_tab_edge = settings.color_scheme.config.background,
+			new_tab = {
+				bg_color = settings.color_scheme.config.foreground,
+				fg_color = settings.color_scheme.config.background,
+			},
+			new_tab_hover = {
+				bg_color = settings.color_scheme.config.foreground,
+				fg_color = settings.color_scheme.config.background,
+			},
+		},
+	},
+
+	window_background_opacity = 0.85,
+	window_frame = {
+		active_titlebar_bg = settings.color_scheme.config.foreground,
+		inactive_titlebar_bg = settings.color_scheme.config.background,
+	},
+
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	show_new_tab_button_in_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
 }
-
-local config = {
-    color_scheme = settings.color_scheme.name,
-    window_background_opacity = 0.95,
-    colors = {
-        tab_bar = {
-            background = settings.color_scheme.config.background,
-            active_tab = {
-                bg_color = settings.color_scheme.config.background,
-                fg_color = settings.color_scheme.config.foreground
-            },
-            inactive_tab = {
-                bg_color = settings.color_scheme.config.background,
-                fg_color = settings.color_scheme.config.foreground
-            },
-            inactive_tab_hover = {
-                bg_color = settings.color_scheme.config.background,
-                fg_color = settings.color_scheme.config.foreground
-            },
-            inactive_tab_edge = settings.color_scheme.config.background,
-            new_tab = {
-                bg_color = settings.color_scheme.config.foreground,
-                fg_color = settings.color_scheme.config.background
-            },
-            new_tab_hover = {
-                bg_color = settings.color_scheme.config.foreground,
-                fg_color = settings.color_scheme.config.background
-            }
-        }
-    },
-    window_frame = {
-        active_titlebar_bg = settings.color_scheme.config.foreground,
-        inactive_titlebar_bg = settings.color_scheme.config.background
-    }
-}
-
-return config
